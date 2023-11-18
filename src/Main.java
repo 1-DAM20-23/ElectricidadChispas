f13dd05addf0439abf97e7c879e92c940def66e9
ivanblancosantamaria <ivan.blanco.movil@gmail.com>
2023-11-18 16:04:45 +0100

    Punto de control

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static Scanner scnTeclado;
 
+    public static void main(String[] args) {
         //BBDD
         ArrayList<Cliente> listaClientes = new ArrayList<>();
         ArrayList<Factura> listaFacaturas = new ArrayList<>();
         ArrayList<Producto> listaProductos = new ArrayList<>();
         ArrayList<Servicio> lsitaServicios = new ArrayList<>();
         //VARIABLES
-
         int opcion =0;
+        String dato;
         //MENU
         do {
 
@@ -34,15 +35,16 @@ public class Main {
 
             switch (opcion){
                 case 1: {
-                    System.out.println(listaClientes);
+                    for(int i=0 ; listaClientes.size();i++){
+                        System.out.println(listaClientes.toString());
+                    }
                 }break;
                 case 2:{
-                    System.out.println(listaProductos);
+
                 }break;
                 case 3:{
-                    System.out.println(listaFacaturas);
-                }break;
 
+                }break;
                 case 4:{
                     System.out.println("Datos de cliente:");
                     System.out.println("---------------------------------------");
@@ -54,63 +56,121 @@ public class Main {
 
                     switch (opcion){
                         case 1:{
-                            /*
-                            System.out.println("Nombre:");
-                            scnTeclado = new Scanner(System.in);
-                             String dato = scnTeclado.nextLine();
-                            new Autonomo().setNombre(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Apellidos:");
+                            listaClientes.add(leerAutonomo());
                             scnTeclado = new Scanner(System.in);
                             dato = scnTeclado.nextLine();
-                            new Autonomo().setApellidos(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Dni:");
-                            scnTeclado = new Scanner(System.in);
-                            dato = scnTeclado.nextLine();
-                            new Autonomo().setDni(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Direccion Postal:");
-                            scnTeclado = new Scanner(System.in);
-                            dato = scnTeclado.nextLine();
-                            new Autonomo().setDireccionPostal(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Poblacion:");
-                            scnTeclado = new Scanner(System.in);
-                            dato = scnTeclado.nextLine();
-                            new Autonomo().setPoblacion(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Provincia:");
-                            scnTeclado = new Scanner(System.in);
-                            dato = scnTeclado.nextLine();
-                            new Autonomo().setProvincia(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Email:");
-                            scnTeclado = new Scanner(System.in);
-                            dato = scnTeclado.nextLine();
-                            new Autonomo().setEmail(dato);
-                            listaClientes.add(dato);
-                            System.out.println("Telefono:");
-                            scnTeclado = new Scanner(System.in);
-                            dato = scnTeclado.nextLine();
-                            new Autonomo().setApellidos(dato);
-                            listaClientes.add(dato);
-                            */
+                            listaClientes.add(leerAutonomo());
                         }
                         case 2:{
-                            System.out.println("Nombre empresa:");
-                            System.out.println("Cif:");
-                            System.out.println("Direccion Postal:");
-                            System.out.println("Poblacion:");
-                            System.out.println("Provincia:");
-                            System.out.println("Email:");
-                            System.out.println("Apellidos:");
-                            System.out.println("Telefono:");
+                            listaClientes.add(leerSociedad());
+                            scnTeclado = new Scanner(System.in);
+                            dato = scnTeclado.nextLine();
+                            listaClientes.add(leerSociedad());
                         }
                     }
                 }break;
+                case 5:{
+                    System.out.println("Cliente a borrar:");
+                    System.out.println("---------------------------------------");
+                    do {
+                        int contador =0;
+                        System.out.print(listaClientes.get(contador));
+                        System.out.print(listaClientes.get(leerAutonomo().getCodgioCliente()));
+                        System.out.print(listaClientes.get(leerAutonomo().getCodgioCliente()));
+                        contador++;
+                    }while (listaClientes<listaClientes.size());
+
+                    }
+
+                    scnTeclado = new Scanner(System.in);
+                    dato = scnTeclado.nextLine();
+                    if (posicion >= 0 && posicion < listaClientes.size() ){
+                        listaClientes.remove(dato);
+                    }else
+
+
+
+
+                }break;
+
             }
         }while (opcion!=0);
     }
 
+
+    public static Cliente leerAutonomo(){
+        Autonomo autonomoNuevo = new Autonomo();
+        System.out.println("nombre:");
+        String dato = scnTeclado.nextLine();
+        autonomoNuevo.setNombre(dato);
+
+        System.out.println("Apellidos:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setApellidos(dato);
+
+        System.out.println("Apellidos:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setApellidos(dato);
+
+        System.out.println("Dni:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setDni(dato);
+
+        System.out.println("Direccion postal:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setDireccionPostal(dato);
+
+        System.out.println("Poablacion:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setPoblacion(dato);
+
+        System.out.println("Provincia:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setProvincia(dato);
+
+        System.out.println("Email:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setEmail(dato);
+
+        System.out.println("Telefono:");
+        dato = scnTeclado.nextLine();
+        autonomoNuevo.setTelefono(dato);
+
+        return autonomoNuevo;
+    }
+
+    public static Cliente leerSociedad(){
+        Sociedad sociedadNueva = new Sociedad();
+        System.out.println("Razon social:");
+        String dato = scnTeclado.nextLine();
+        sociedadNueva.setRazonSocial(dato);
+
+        System.out.println("Cif:");
+        dato = scnTeclado.nextLine();
+        sociedadNueva.setCif(dato);
+
+        System.out.println("Direccion postal:");
+        dato = scnTeclado.nextLine();
+        sociedadNueva.setDireccionPostal(dato);
+
+        System.out.println("Poblacion:");
+        dato = scnTeclado.nextLine();
+        sociedadNueva.setPoblacion(dato);
+
+        System.out.println("Provincia:");
+        dato = scnTeclado.nextLine();
+        sociedadNueva.setProvincia(dato);
+
+        System.out.println("Emamil:");
+        dato = scnTeclado.nextLine();
+        sociedadNueva.setEmail(dato);
+
+        System.out.println("Telefonmo:");
+        dato = scnTeclado.nextLine();
+        sociedadNueva.setTelefono(dato);
+        return sociedadNueva;
+    }
+    public static Cliente borrarCliente(){
+
+    }
 }
